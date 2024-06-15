4985d8d9e03296d23a722d741815eac7b56b0c02
gomhyeok <gomhyeok@gmail.com>
2024-06-15 15:55:17 +0900

    [DOCS] gitignore 파일 추가

---
+++ MyKkumi/MyKkumi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyKkumi
+//
+//  Created by 최재혁 on 6/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
