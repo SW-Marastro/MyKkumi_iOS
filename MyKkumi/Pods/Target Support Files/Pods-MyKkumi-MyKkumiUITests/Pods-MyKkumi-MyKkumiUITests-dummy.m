6bb80efb034ead9ada185899b5e27110805f69de
GomHyeok <102780858+GomHyeok@users.noreply.github.com>
2024-06-24 18:58:59 +0900

    Delete MyKkumi/Pods directory

--- MyKkumi/Pods/Target Support Files/Pods-MyKkumi-MyKkumiUITests/Pods-MyKkumi-MyKkumiUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyKkumi_MyKkumiUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MyKkumi_MyKkumiUITests
-@end
--- MyKkumi/Pods/Target Support Files/Pods-MyKkumiTests/Pods-MyKkumiTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyKkumiTests : NSObject
-@end
-@implementation PodsDummy_Pods_MyKkumiTests
-@end
