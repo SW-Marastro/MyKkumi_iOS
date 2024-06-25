ed30e0a03eff5458cb83f0cdfc89bf0d7264f2f7
gomhyeok <gomhyeok@gmail.com>
2024-06-25 17:28:36 +0900

    KKUMI-18 [FEAT] #10 - 상단바 UI 요소 생성 - 각 아이템 버튼 생성 - textfiled생성, 동작은 하지 않음

--- MyKkumi/MyKkumi/Presentation/Home/HomeViewController.swift
+++ MyKkumi/MyKkumi/Presentation/Home/HomeViewController.swift
@@ -9,8 +9,82 @@ import Foundation
 import RxSwift
 
 class HomeViewController : UIViewController {
+    var viewModel : HomeViewModel
+    private let disposeBag = DisposeBag()
+    
+    private lazy var hamburgurButton : UIButton = {
+        let button = UIButton()
+        button.isEnabled = true
+        button.backgroundColor = .white
+        button.setBackgroundImage(UIImage(named: "Hamburgur"), for: .normal)
+        
+        return button
+    }()
+    
+    private lazy var searchText : UITextField = {
+        let textfield = UITextField();
+        textfield.frame = CGRect.zero
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        textfield.autocapitalizationType = .none
+        textfield.autocorrectionType = .no
+        textfield.placeholder = "마이구미 통합검색"
+        textfield.clearButtonMode = .always
+        textfield.clearsOnBeginEditing = false
+        textfield.backgroundColor = Colors.GrayColor
+        textfield.delegate = self
+        
+        return textfield
+    }()
+    
+    private lazy var searchButton : UIButton = {
+        let button = UIButton()
+         button.isEnabled = true
+         button.backgroundColor = .white
+         button.setBackgroundImage(UIImage(named: "Search"), for: .normal)
+         
+         return button
+     }()
+    
+    private lazy var notificationButton : UIButton = {
+       let button = UIButton()
+        button.isEnabled = true
+        button.backgroundColor = .white
+        button.setBackgroundImage(UIImage(named: "Notify"), for: .normal)
+        
+        return button
+    }()
+    
+    private lazy var shoppingCartButton : UIButton = {
+       let button = UIButton()
+        button.isEnabled = true
+        button.backgroundColor = .white
+        button.setBackgroundImage(UIImage(named: "ShoppingCart"), for: .normal)
+        
+        return button
+    }()
+    
+    public init(viewModel: HomeViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
+extension HomeViewController : UITextFieldDelegate {
+    //return 버튼 눌리면 검색 API 호출하도록 수정해야함
+    func textFieldShouldReturn(_ textField : UITextField) -> Bool {
+        return true
     }
 }
